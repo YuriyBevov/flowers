5037a00c4ef24a07d60642cfa646a9a15b96451e
yuriybevov <yuriybevov@gmail.com>
2024-03-03 22:40:43 +0300

    catalog

--- src/scripts/modules/catalogTileCardSquares.js
+++ src/scripts/modules/catalogTileCardSquares.js
@@ -1,4 +1,4 @@
-const containers = document.querySelectorAll(".catalog-tile-card__header");
+const containers = document.querySelectorAll(".catalog-tile-card__top");
 
 if (containers) {
   const COUNT = 3;
@@ -6,13 +6,13 @@ if (containers) {
   containers.forEach((container) => {
     const squareBox = document.createElement("div");
     squareBox.classList.add("catalog-tile-card-square-box");
-  
+
     for (let i = 0; i < COUNT; i++) {
       const square = document.createElement("div");
       square.classList.add("catalog-tile-card-square");
       squareBox.append(square);
     }
-  
+
     container.append(squareBox);
   });
 }
